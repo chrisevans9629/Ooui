cd1755a6879510407b600b87fb8585b67adf4767
Chris Evans <chrisevans9629@gmail.com>
2019-07-10 22:12:09 -0500

    added xamarin forms app with prism to test with ooui

---
+++ PrismSample.Wasm/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Ooui;
+using Xamarin.Forms;
+
+namespace PrismSample.Wasm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Forms.Init();
+
+            var app = new App();
+
+            UI.Publish("/", app.MainPage.GetOouiElement());
+            Console.ReadLine();
+        }
+    }
+}
